3c5b225dd10f8a2d02efe71ef194abf442818994
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-02 17:45:43 -0300

    feat: cria migration cliente

---
+++ src/migrations/20220502180512-create-clientes.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Clientes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Clientes');
+  }
+};
