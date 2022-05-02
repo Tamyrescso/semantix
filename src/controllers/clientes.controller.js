12f442df4360ceba91e98eb0a7daec829e095c6b
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-02 19:16:11 -0300

    feat: add o create, update e delete de clients

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -10,6 +10,44 @@ const list = async (_req, res, next) => {
   }
 };
 
+const create = async (req, res, next) => {
+  try {
+    const { nome, email } = req.body;
+    const { code, data } = await clientesService.create(nome, email);
+
+    return res.status(code).json(data);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const update = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { nome, email } = req.body;
+
+    const { code, data } = await clientesService.update(nome, email, id);
+
+    return res.status(code).json(data);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const destroy = async (req, res, next) => {
+  try {
+  const { id } = req.params;
+  const { code } = await clientesService.destroy(id);
+
+  return res.status(code).end();
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
   list,
+  create,
+  update,
+  destroy
 }
--- src/services/clientes.service.js
+++ src/services/clientes.service.js
@@ -1,4 +1,5 @@
 const { Clientes } = require('../models');
+const validateClient = require('./schema/clientes.validation');
 
 const list = async () => {
   const clients = await Clientes.findAll();
@@ -6,6 +7,39 @@ const list = async () => {
   return { code: 200, data: clients };
 };
 
+const create =  async (nome, email) => {
+  const error = validateClient(nome, email);
+  if (error) return { code: 400, data: { message: error } };
+
+  const alreadyExists = await Clientes.findOne({ where: { email } });
+  if (alreadyExists) return { code: 409, data: { message: 'Client already registered' } };
+
+  console.log(error, alreadyExists)
+  const newClient = await Clientes.create({nome, email});
+
+  return { code: 201, data: newClient };
+};
+
+const update = async (nome, email, id) => {
+  const error = validateClient(nome, email);
+  if (error) return { code: 400, data: { message: error } };
+
+  await Clientes.update({ nome, email }, { where: { id } });
+
+  const updated = await Clientes.findByPk(id);
+
+  return { code: 200, data: updated };
+};
+
+const destroy = async (id) => {
+  await Clientes.destroy({ where: { id } });
+
+  return { code: 204 };
+};
+
 module.exports = {
   list,
-}
+  create,
+  update,
+  destroy,
+};
