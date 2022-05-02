be6f590435e7d47398a71423c80b659c945a729d
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-02 17:44:54 -0300

    cria model produtos

---
+++ src/models/produtos.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  quantidadeEstoque: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  preço: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+}
+module.exports = (sequelize) => {
+  const Produtos = sequelize.define(
+    'Produtos',
+    Attributes,
+    {
+      modelName: 'Produtos',
+      timestamps: false,
+      underscored: false,
+    }
+  );
+  return Produtos;
+};
