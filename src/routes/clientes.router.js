86422f4d187e0a76d74c6528c6f89e4334187bf5
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-02 19:16:54 -0300

    feat: add métodos post, put e delete da rota clientes

--- src/routes/clientes.router.js
+++ src/routes/clientes.router.js
@@ -4,5 +4,8 @@ const clientesController = require('../controllers/clientes.controller');
 const clientesRouter = express.Router();
 
 clientesRouter.get('/', clientesController.list);
+clientesRouter.post('/', clientesController.create);
+clientesRouter.put('/:id', clientesController.update);
+clientesRouter.delete('/:id', clientesController.destroy);
 
 module.exports = clientesRouter;
