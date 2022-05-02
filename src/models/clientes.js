ecc646e86bb06073a90515df6596d58ae5da5da5
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-02 17:44:25 -0300

    feat: cria model clientes

---
+++ src/models/clientes.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nome: {
+    type: DataTypes.STRING, 
+    allowNull: false,
+  },
+  nome: {
+    type: DataTypes.STRING, 
+    allowNull: false,
+  },
+}
+module.exports = (sequelize) => {
+  const Clientes = sequelize.define(
+    'Clientes',
+    Attributes,
+    {
+      modelName: 'Clientes',
+      timestamps: false,
+      underscored: false,
+    }
+  );
+
+  Clientes.associate = (models) => {
+    Clientes.hasMany(models.Compras, { foreignKey: 'clienteId', as: 'compra' });
+  };
+  return Clientes;
+};
