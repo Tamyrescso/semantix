f8985e1f0dc87b37b52d822608f7f4242c7c5aa6
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-04 03:47:25 -0300

    fix: conserta validação de compras

--- src/services/validations/compras.validation.js
+++ src/services/validations/compras.validation.js
@@ -4,27 +4,29 @@ const { genericNumberValidation, genericLengthValidation } = require('./generic.
 const validateOrder = (clienteId, compras, clienteOuCompra) => {
   const errorClient = genericNumberValidation(clienteId, clienteOuCompra);
   const errorOrder = genericLengthValidation(compras, 'compras');
-  const errorProductsId = compras.map((o) => genericNumberValidation(o.produtoId, 'produtoId'));
-  const errorQuantity = compras.map((o) => genericNumberValidation(o.quantidade, 'quantidade'));
 
   if (errorClient) return errorClient;
   if (errorOrder) return errorOrder;
+
+  const errorProductsId = compras.map((o) => genericNumberValidation(o.produtoId, 'produtoId'));
+  const errorQuantity = compras.map((o) => genericNumberValidation(o.quantidade, 'quantidade'));
+
   if (errorProductsId.some((e) => e)) return errorProductsId.find((e) => e);
-  if (errorQuantity.some((e) => e)) return errorProductsId.find((e) => e);
+  if (errorQuantity.some((e) => e)) return errorQuantity.find((e) => e);
 
   return null;
 }
 
 const ifClientExist = async (clienteId) => {
   const clientExists = await Clientes.findByPk(clienteId);
-  if (!clientExists) return { code: 409, data: { message: "Client doesn't exist" } };
+  if (!clientExists) return { code: 404, data: { message: "Client doesn't exist" } };
 
   return null;
 };
 
 const ifOrderExist = async (compraId) => {
   const orderExists = await Compras.findByPk(compraId);
-  if (!orderExists) return { code: 409, data: { message: "Order doesn't exist" } };
+  if (!orderExists) return { code: 404, data: { message: "Order doesn't exist" } };
 
   return null;
 }
@@ -34,7 +36,7 @@ const ifProductsExist = async (compras) => {
     return Produtos.findByPk(order.produtoId)
   }));
 
-  if(productNotExist.some((product) => !product)) return { code: 409, data: { message: "Product doesn't exist" } }
+  if(productNotExist.some((product) => !product)) return { code: 404, data: { message: "Product doesn't exist" } }
 }
 
 module.exports = {
