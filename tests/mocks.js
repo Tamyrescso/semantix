b6fd813de3bd9337c66d1a300b1f6726ed50f815
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-03 22:43:14 -0300

    test: add mocks para testar rota /clientes

---
+++ tests/mocks.js
@@ -0,0 +1,34 @@
+const clients = [
+  {
+    "id": 1,
+    "nome": "Lewis Hamilton",
+    "email": "lewishamilton@gmail.com",
+    "telefone": "(45)95876-5434"
+  },
+  {
+    "id": 2,
+    "nome": "Michael Schumacher",
+    "email": "MichaelSchumacher@gmail.com",
+    "telefone": "(24)2345-9485"
+  }
+];
+
+const createClient = {
+  "id": 3,
+  "nome": "Michael Jackson",
+  "email": "michael.mj@gmail.com",
+  "telefone": "(72)8345-4554"
+};
+
+const updateClient = {
+	"id": 2,
+	"nome": "Michael Schumacher",
+	"email": "MichaelSchumacher@gmail.com",
+	"telefone": "(24)0000-0000"
+}
+
+module.exports = {
+  clients,
+  createClient,
+  updateClient
+}
