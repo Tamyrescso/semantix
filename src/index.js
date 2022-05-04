2d4c72e39422cb5d223a0f99fedd44899891d4d7
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-04 04:39:45 -0300

    fix: altera porta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
