9255bbf8c78e6455a4cc4bb63aac27b11b1bbdce
Tamyres <tamyrescaroline.oliveira@gmail.com>
2022-05-04 03:44:30 -0300

    test: add mocks de compras

--- tests/mocks.js
+++ tests/mocks.js
@@ -56,11 +56,175 @@ const createProduct = {
 };
 
 const updateProduct = {
-  "id": 2,
-  "nome": "Traje de encolhimento",
-  "quantidadeEstoque": 26,
-  "preço": "300.00"
+  "id": 3,
+  "nome": "Escudo do Capitão América",
+  "quantidadeEstoque": 50,
+  "preço": "400.00"
 };
+
+
+const orders = [
+	{
+		"id": 1,
+		"createdAt": "2011-08-01T19:58:00.000Z",
+		"updatedAt": "2022-05-03T21:07:38.866Z",
+		"cliente": {
+			"id": 2,
+			"nome": "Michael Schumacher",
+			"email": "MichaelSchumacher@gmail.com",
+			"telefone": "(24)0000-0000"
+		},
+		"produtos": [
+			{
+				"id": 2,
+				"nome": "Traje de encolhimento",
+				"preço": "300.00",
+				"ComprasProdutos": {
+					"quantidade": 10
+				}
+			},
+			{
+				"id": 3,
+				"nome": "Escudo do Capitão América",
+				"preço": "400.00",
+				"ComprasProdutos": {
+					"quantidade": 5
+				}
+			},
+			{
+				"id": 1,
+				"nome": "Martelo do Thor",
+				"preço": "500.00",
+				"ComprasProdutos": {
+					"quantidade": 10
+				}
+			}
+		]
+	},
+	{
+		"id": 2,
+		"createdAt": "2011-04-01T19:58:00.000Z",
+		"updatedAt": null,
+		"cliente": {
+			"id": 2,
+			"nome": "Michael Schumacher",
+			"email": "MichaelSchumacher@gmail.com",
+			"telefone": "(24)0000-0000"
+		},
+		"produtos": [
+			{
+				"id": 1,
+				"nome": "Martelo do Thor",
+				"preço": "500.00",
+				"ComprasProdutos": {
+					"quantidade": 2
+				}
+			}
+		]
+	},
+	{
+		"id": 3,
+		"createdAt": "2010-03-01T19:58:00.000Z",
+		"updatedAt": null,
+		"cliente": {
+			"id": 1,
+			"nome": "Lewis Hamilton",
+			"email": "lewishamilton@gmail.com",
+			"telefone": "(45)95876-5434"
+		},
+		"produtos": [
+			{
+				"id": 2,
+				"nome": "Traje de encolhimento",
+				"preço": "300.00",
+				"ComprasProdutos": {
+					"quantidade": 5
+				}
+			}
+		]
+	},
+	{
+		"id": 4,
+		"createdAt": "2022-05-03T20:37:06.924Z",
+		"updatedAt": "2022-05-03T20:37:06.924Z",
+		"cliente": {
+			"id": 2,
+			"nome": "Michael Schumacher",
+			"email": "MichaelSchumacher@gmail.com",
+			"telefone": "(24)0000-0000"
+		},
+		"produtos": [
+			{
+				"id": 2,
+				"nome": "Traje de encolhimento",
+				"preço": "300.00",
+				"ComprasProdutos": {
+					"quantidade": 2
+				}
+			}
+		]
+	}
+];
+
+const updateOrder = {
+    "id": 2,
+    "clienteId": 2,
+    "createdAt": "2011-04-01T19:58:00.000Z",
+    "updatedAt": "2011-04-01T19:58:00.000Z",
+};
+
+const orderByClient = [
+	{
+		"id": 1,
+		"createdAt": "2011-08-01T19:58:00.000Z",
+		"updatedAt": null,
+		"cliente": {
+			"id": 1,
+			"nome": "Lewis Hamilton",
+			"email": "lewishamilton@gmail.com",
+			"telefone": "(45)95876-5434"
+		},
+		"produtos": [
+			{
+				"id": 2,
+				"nome": "Traje de encolhimento",
+				"preço": "300.00",
+				"ComprasProdutos": {
+					"quantidade": 10
+				}
+			},
+			{
+				"id": 3,
+				"nome": "Escudo do Capitão América",
+				"preço": "400.00",
+				"ComprasProdutos": {
+					"quantidade": 5
+				}
+			}
+		]
+	},
+	{
+		"id": 3,
+		"createdAt": "2010-03-01T19:58:00.000Z",
+		"updatedAt": null,
+		"cliente": {
+			"id": 1,
+			"nome": "Lewis Hamilton",
+			"email": "lewishamilton@gmail.com",
+			"telefone": "(45)95876-5434"
+		},
+		"produtos": [
+			{
+				"id": 2,
+				"nome": "Traje de encolhimento",
+				"preço": "300.00",
+				"ComprasProdutos": {
+					"quantidade": 5
+				}
+			}
+		]
+	}
+];
 
 module.exports = {
   clients,
@@ -68,5 +232,8 @@ module.exports = {
   updateClient,
   products,
   createProduct,
-  updateProduct
+  updateProduct,
+  orders,
+  updateOrder,
+  orderByClient
 }
